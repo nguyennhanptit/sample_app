ef3f5c8c34d0a676b6d266b5196b5620038a2efe
Nguyen Nhan <thanhnhannguyen121001@gmail.com>
2018-07-10 09:34:46 +0700

    Finish static pages

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
-  
+  root "static_pages#home"
+  get "static_pages/about"
+  get "static_pages/home"
+  get "static_pages/help"
+  scope "(:locale)", locale: /en|vi/ do
+    root to: 'welcome#index'
+  end
 end
