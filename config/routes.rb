34dfdda6d1a23e2f0c14d39e107b0c5aca7b26e6
Nguyen Nhan <thanhnhannguyen121001@gmail.com>
2018-07-10 09:34:46 +0700

    Filling in the layout

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,10 @@
 Rails.application.routes.draw do
+  get 'users/new'
   root "static_pages#home"
-  get "static_pages/about"
-  get "static_pages/home"
-  get "static_pages/help"
+  get "/about", to: "static_pages#about"
+  get "/help", to: "static_pages#help"
+  get "/contact", to: "static_pages#contact"
+  get "/signup", to: "users#new"
   scope "(:locale)", locale: /en|vi/ do
     root to: 'welcome#index'
   end
